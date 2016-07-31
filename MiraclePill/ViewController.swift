576ae4926cef858f7f75cfd10f88b802800df46e
Dave McMahon <davidmcmhn@gmail.com>
2016-07-31 16:08:42 +0100

    Initial commit of project

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Dave on 31/07/2016.
+//  Copyright © 2016 Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var regionPicker: UIPickerView!
+    @IBOutlet weak var regionBtn: UIButton!
+    
+    let regions: [String] = ["Munster", "Connaght","Leinster","Ulster"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        regionPicker.dataSource = self
+        regionPicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func regionBtnPressed(_ sender: AnyObject) {
+        regionPicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return regions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return regions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        regionBtn.setTitle(regions[row], for: UIControlState.normal)
+        regionPicker.isHidden = true
+    }
+
+}
+
